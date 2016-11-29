1333b0657309ed454f721395a3948ac7df687aa2
Leigh Anderson <landerson@equalexperts.com>
2016-11-29 08:19:25 +0000

    [LA][APB-408] Add IP whitelist

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -25,6 +25,7 @@ private object AppDependencies {
   private val scalaTestVersion = "2.2.6"
   private val pegdownVersion = "1.6.0"
   private val playHmrcApiVersion = "1.2.0"
+  private val playWhitelistVersion = "2.0.0"
 
 
   val compile = Seq(
@@ -37,7 +38,8 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
     "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
-    "uk.gov.hmrc" %% "play-hmrc-api" % playHmrcApiVersion
+    "uk.gov.hmrc" %% "play-hmrc-api" % playHmrcApiVersion,
+    "uk.gov.hmrc" %% "play-whitelist-filter" % playWhitelistVersion
   )
 
   trait TestDependencies {
