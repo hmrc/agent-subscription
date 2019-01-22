af1c7c90642b6f0baa936be7c15ed4191bf9d179
John Revill <john@abatons.org>
2019-01-22 18:11:58 +0000

    [ASA-634][JR] Removing the mandatory telephone number in the agency details

--- it/uk/gov/hmrc/agentsubscription/stubs/OverseasDesStubs.scala
+++ it/uk/gov/hmrc/agentsubscription/stubs/OverseasDesStubs.scala
@@ -36,15 +36,15 @@ trait OverseasDesStubs {
              |}
            """.stripMargin)))
   }
-  def organisationRegistrationFailsWithNotFound(requestJson: String): Unit = {
-    stubFor(maybeWithDesHeaderCheck(registrationRequest(requestJson))
+  def organisationRegistrationFailsWithNotFound(): Unit = {
+    stubFor(maybeWithDesHeaderCheck(post(urlEqualTo(s"/registration/02.00.00/organisation")))
       .willReturn(aResponse()
         .withStatus(404)
         .withBody(notFoundResponse)))
   }
 
-  def organisationRegistrationFailsWithInvalidPayload(requestJson: String): Unit = {
-    stubFor(maybeWithDesHeaderCheck(registrationRequest(requestJson))
+  def organisationRegistrationFailsWithInvalidPayload(): Unit = {
+    stubFor(maybeWithDesHeaderCheck(post(urlEqualTo(s"/registration/02.00.00/organisation")))
       .willReturn(aResponse()
         .withStatus(400)
         .withBody(invalidPayloadResponse)))
