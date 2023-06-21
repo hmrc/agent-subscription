8aafe349a4f2f90e3da13991bfe7d5fb573ce19b
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-06-21 14:22:23 +0100

    live [CS] pr-bot reqs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,16 +11,17 @@ object AppDependencies {
     "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % bootstrapVer,
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"     % "1.10.0",
     "com.github.blemale" %% "scaffeine"                 % "4.0.1",
-    "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"  % "5.3.0",
+    "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"  % "5.3.0" exclude("uk.gov.hmrc", "bootstrap-backend-play-28"),
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"        % mongoVer
   )
   
   lazy val test = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"    % "test, it",
-    "org.scalatestplus"      %% "mockito-3-12"            % "3.2.10.0" % "test, it",
-    "com.github.tomakehurst" %  "wiremock-jre8"           % "2.26.1"   % "test, it",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoVer   % "test, it",
-    "com.vladsch.flexmark"   %  "flexmark-all"            % "0.35.10"  % "test, it"
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"      % "test, it",
+    "org.scalatestplus"      %% "mockito-3-12"            % "3.2.10.0"   % "test, it",
+    "com.github.tomakehurst" %  "wiremock-jre8"           % "2.26.1"     % "test, it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoVer     % "test, it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVer % "test, it",
+    "com.vladsch.flexmark"   %  "flexmark-all"            % "0.35.10"    % "test, it"
   )
 
 }
