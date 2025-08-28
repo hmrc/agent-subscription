e4abc86bbab4900078e8e0440d245310ce61884f
raw1697 <romel.w@hotmail.co.uk>
2025-08-28 16:56:36 +0100

    APB-9841 update AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVer: String = "9.13.0"
-  private val mongoVer: String = "2.6.0"
+  private val bootstrapVer: String = "10.1.0"
+  private val mongoVer: String = "2.7.0"
 
   lazy val compile = Seq(
     "uk.gov.hmrc"        %% "bootstrap-backend-play-30" % bootstrapVer,
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"     % "2.2.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"        % mongoVer,
-    "uk.gov.hmrc"        %% "crypto-json-play-30"       % "8.2.0"
+    "uk.gov.hmrc"        %% "crypto-json-play-30"       % "8.3.0"
   )
   
   lazy val test = Seq(
